31a7217a3c7ff28b510a68039b609ec23962b297
JohnFabu <102181948+JohnFabu@users.noreply.github.com>
2022-04-25 09:56:00 +0200

    led ubuda

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 let led2 = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
-let range = led2.range(0, 2)
-range.showColor(neopixel.colors(NeoPixelColors.Red))
+let range = led2.range(0, 9)
+range.showColor(neopixel.colors(NeoPixelColors.Green))
 for (let index = 0; index < 8; index++) {
-    led2.shift(1)
     led2.show()
+    led2.shift(1)
     basic.pause(1000)
 }
